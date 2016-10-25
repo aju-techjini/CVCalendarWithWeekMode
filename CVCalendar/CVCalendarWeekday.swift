c624d3135d93bf6eb60d08935f20188b185f58b0
Aju Antony <aju@techjini.com>
2016-10-25 18:39:24 +0530

    Added Basic Classes

---
+++ CVCalendar/CVCalendarWeekday.swift
@@ -0,0 +1,24 @@
+//
+//  CVCalendarWeekday.swift
+//  CVCalendar Demo
+//
+//  Created by Eugene Mozharovsky on 15/04/15.
+//  Copyright (c) 2015 GameApp. All rights reserved.
+//
+
+import Foundation
+
+@objc public enum CVCalendarWeekday: Int {
+    case sunday = 1
+    case monday = 2
+    case tuesday = 3
+    case wednesday = 4
+    case thursday = 5
+    case friday = 6
+    case saturday = 7
+}
+
+@objc public enum CVCalendarMode: Int {
+    case fiveday = 1
+    case sevenday = 2
+}
